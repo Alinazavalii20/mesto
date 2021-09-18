e12385727039926e364e1bedccc71566cc357a00
Alinazavalii20 <87885095+Alinazavalii20@users.noreply.github.com>
2021-09-18 21:02:46 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -9,28 +9,26 @@ let jobInput = popup.querySelector('.popup__input_userjob');
 let nameProfile = document.querySelector('.profile__name');
 let subtitleProfile = document.querySelector('.profile__subtitle');
 
-// Открытие попапа
+
 function popupOpen() {
   popup.classList.add('popup__opened');
   nameInput.value = nameProfile.textContent;
   jobInput.value = subtitleProfile.textContent;
 }
-// Закрытие попапа
+
 function popupClose() {
   popup.classList.remove('popup__opened');
 }
-// Сохранение в форме ввода данных (перезапись полей профиля и закрытие попапа)
+
 function formSubmitHandler(evt) {
-  evt.preventDefault(); // отмена стандартной отправки формы
+  evt.preventDefault(); 
   nameProfile.textContent = nameInput.value;
   subtitleProfile.textContent = jobInput.value;
   popupClose();
 }
 
-// Отслеживаем клики на кнопках открытия/закрытия попапа
 popupOpenBtn.addEventListener('click', popupOpen);
 popupCloseBtn.addEventListener('click', popupClose);
 
-// Отслеживаем клик на кнопке Сохранить в форме ввода данных
 form.addEventListener('submit', formSubmitHandler);
 formSubmitHandler();
